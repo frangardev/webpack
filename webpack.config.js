4352968e616d807f161451f7070ccc47c34d659f
Francisco García <francoffee57@gmail.com>
2021-11-20 17:24:51 -0600

    Configuré webpack para HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,14 @@
-//Lo primero es traer path (path ya viene con node):
 const path = require('path');
-//Creamos un modulo que vamos a exportar con la configuración deceada:
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-    //Entri es el punto de entrada de nuestra aplicacion, es decir el main o el index
     entry: "./src/index.js",
-    //Ouput es hacia donde va mandar webpack lo que haga, por defecto es la carpeta dist
     output:{
-         // path es donde estará la carpeta donde se guardará los archivos
-        // Con path.resolve podemos decir dónde va estar la carpeta y la ubicación del mismo
         path: path.resolve(__dirname, "dist"),
-        // filename le pone el nombre al archivo final
+
         filename: "main.js",
     },
-    //Que va a trabajar
     resolve :{
-        // Aqui ponemos las extensiones que tendremos en nuestro proyecto para webpack los lea
         extensions : [".js"]
     },
     module : {
@@ -28,4 +22,11 @@ module.exports = {
             }
         ]
     },
+    plugins : [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ]
 }
